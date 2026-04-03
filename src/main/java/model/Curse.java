9c870aac441257f1210e427d1cdd2053bfe6c87d
vitaliymaksimovich <cherkasovfr@yandex.ru>
2026-04-03 09:19:59 +0300

    model: change domain classes for new structure

--- src/main/java/model/Curse.java
+++ src/main/java/model/Curse.java
@@ -4,35 +4,19 @@ public class Curse {
     private String name;
     private String threatLevel;
 
-    public Curse() {
-    }
-
+    public Curse() {}
     public Curse(String name, String threatLevel) {
         this.name = name;
         this.threatLevel = threatLevel;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getThreatLevel() {
-        return threatLevel;
-    }
-
-    public void setThreatLevel(String threatLevel) {
-        this.threatLevel = threatLevel;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getThreatLevel() { return threatLevel; }
+    public void setThreatLevel(String threatLevel) { this.threatLevel = threatLevel; }
 
     @Override
     public String toString() {
-        return "Curse{" +
-                "name='" + name + '\'' +
-                ", threatLevel='" + threatLevel + '\'' +
-                '}';
+        return "Curse{name='" + name + "', threatLevel='" + threatLevel + "'}";
     }
 }
--- src/main/java/model/Mission.java
+++ src/main/java/model/Mission.java
@@ -1,13 +1,10 @@
 package model;
 
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Mission {
-
+    // Обязательные поля
     private String missionId;
     private String date;
     private String location;
@@ -15,150 +12,73 @@ public class Mission {
     private double damageCost;
     private Curse curse;
 
-    // комментарий миссии (поддерживается TXT / JSON / XML)
+    // Опциональные стандартные блоки
+    private List<Sorcerer> sorcerers = new ArrayList<>();
+    private List<Technique> techniques = new ArrayList<>();
     private String comment;
 
-    // строки, которые не удалось распарсить
-    private List<String> unparsedData;
+    // Расширенные блоки (Приложение 1)
+    private EconomicAssessment economicAssessment;
+    private CivilianImpact civilianImpact;
+    private EnemyActivity enemyActivity;
+    private EnvironmentConditions environmentConditions;
+    private List<OperationEvent> operationTimeline = new ArrayList<>();
+    private List<String> operationTags = new ArrayList<>();
+    private List<String> supportUnits = new ArrayList<>();
+    private List<String> recommendations = new ArrayList<>();
+    private List<String> notes = new ArrayList<>();
+    private List<String> artifactsRecovered = new ArrayList<>();
+    private List<String> evacuationZones = new ArrayList<>();
+    private List<String> statusEffects = new ArrayList<>();
 
-    @JacksonXmlElementWrapper(localName = "sorcerers")
-    @JacksonXmlProperty(localName = "sorcerer")
-    private List<Sorcerer> sorcerers;
+    // Данные, которые не удалось распознать
+    private List<String> unparsedData = new ArrayList<>();
 
-    @JacksonXmlElementWrapper(localName = "techniques")
-    @JacksonXmlProperty(localName = "technique")
-    private List<Technique> techniques;
+    // --- getters/setters ---
 
-    public Mission() {
-        this.sorcerers = new ArrayList<>();
-        this.techniques = new ArrayList<>();
-        this.unparsedData = new ArrayList<>();
-    }
-
-    public Mission(String missionId,
-                   String date,
-                   String location,
-                   String outcome,
-                   double damageCost,
-                   Curse curse,
-                   List<Sorcerer> sorcerers,
-                   List<Technique> techniques,
-                   String comment,
-                   List<String> unparsedData) {
-
-        this.missionId = missionId;
-        this.date = date;
-        this.location = location;
-        this.outcome = outcome;
-        this.damageCost = damageCost;
-        this.curse = curse;
-
-        this.sorcerers = sorcerers != null ? sorcerers : new ArrayList<>();
-        this.techniques = techniques != null ? techniques : new ArrayList<>();
-
-        this.comment = comment;
-        this.unparsedData = unparsedData != null ? unparsedData : new ArrayList<>();
-    }
-
-    public String getMissionId() {
-        return missionId;
-    }
-
-    public void setMissionId(String missionId) {
-        this.missionId = missionId;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getOutcome() {
-        return outcome;
-    }
-
-    public void setOutcome(String outcome) {
-        this.outcome = outcome;
-    }
-
-    public double getDamageCost() {
-        return damageCost;
-    }
-
-    public void setDamageCost(double damageCost) {
-        this.damageCost = damageCost;
-    }
-
-    public Curse getCurse() {
-        return curse;
-    }
-
-    public void setCurse(Curse curse) {
-        this.curse = curse;
-    }
-
-    public List<Sorcerer> getSorcerers() {
-        return sorcerers;
-    }
-
-    public void setSorcerers(List<Sorcerer> sorcerers) {
-        this.sorcerers = sorcerers != null ? sorcerers : new ArrayList<>();
-    }
-
-    public List<Technique> getTechniques() {
-        return techniques;
-    }
-
-    public void setTechniques(List<Technique> techniques) {
-        this.techniques = techniques != null ? techniques : new ArrayList<>();
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public List<String> getUnparsedData() {
-        return unparsedData;
-    }
-
-    public void setUnparsedData(List<String> unparsedData) {
-        this.unparsedData = unparsedData != null ? unparsedData : new ArrayList<>();
-    }
-
-    public void addUnparsedData(String value) {
-        if (value != null && !value.isBlank()) {
-            this.unparsedData.add(value);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Mission{" +
-                "missionId='" + missionId + '\'' +
-                ", date='" + date + '\'' +
-                ", location='" + location + '\'' +
-                ", outcome='" + outcome + '\'' +
-                ", damageCost=" + damageCost +
-                ", curse=" + curse +
-                ", sorcerers=" + sorcerers +
-                ", techniques=" + techniques +
-                ", comment='" + comment + '\'' +
-                ", unparsedData=" + unparsedData +
-                '}';
-    }
+    public String getMissionId() { return missionId; }
+    public void setMissionId(String missionId) { this.missionId = missionId; }
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+    public String getOutcome() { return outcome; }
+    public void setOutcome(String outcome) { this.outcome = outcome; }
+    public double getDamageCost() { return damageCost; }
+    public void setDamageCost(double damageCost) { this.damageCost = damageCost; }
+    public Curse getCurse() { return curse; }
+    public void setCurse(Curse curse) { this.curse = curse; }
+    public List<Sorcerer> getSorcerers() { return sorcerers; }
+    public void setSorcerers(List<Sorcerer> sorcerers) { this.sorcerers = sorcerers != null ? sorcerers : new ArrayList<>(); }
+    public List<Technique> getTechniques() { return techniques; }
+    public void setTechniques(List<Technique> techniques) { this.techniques = techniques != null ? techniques : new ArrayList<>(); }
+    public String getComment() { return comment; }
+    public void setComment(String comment) { this.comment = comment; }
+    public EconomicAssessment getEconomicAssessment() { return economicAssessment; }
+    public void setEconomicAssessment(EconomicAssessment economicAssessment) { this.economicAssessment = economicAssessment; }
+    public CivilianImpact getCivilianImpact() { return civilianImpact; }
+    public void setCivilianImpact(CivilianImpact civilianImpact) { this.civilianImpact = civilianImpact; }
+    public EnemyActivity getEnemyActivity() { return enemyActivity; }
+    public void setEnemyActivity(EnemyActivity enemyActivity) { this.enemyActivity = enemyActivity; }
+    public EnvironmentConditions getEnvironmentConditions() { return environmentConditions; }
+    public void setEnvironmentConditions(EnvironmentConditions environmentConditions) { this.environmentConditions = environmentConditions; }
+    public List<OperationEvent> getOperationTimeline() { return operationTimeline; }
+    public void setOperationTimeline(List<OperationEvent> operationTimeline) { this.operationTimeline = operationTimeline != null ? operationTimeline : new ArrayList<>(); }
+    public List<String> getOperationTags() { return operationTags; }
+    public void setOperationTags(List<String> operationTags) { this.operationTags = operationTags; }
+    public List<String> getSupportUnits() { return supportUnits; }
+    public void setSupportUnits(List<String> supportUnits) { this.supportUnits = supportUnits; }
+    public List<String> getRecommendations() { return recommendations; }
+    public void setRecommendations(List<String> recommendations) { this.recommendations = recommendations; }
+    public List<String> getNotes() { return notes; }
+    public void setNotes(List<String> notes) { this.notes = notes; }
+    public List<String> getArtifactsRecovered() { return artifactsRecovered; }
+    public void setArtifactsRecovered(List<String> artifactsRecovered) { this.artifactsRecovered = artifactsRecovered; }
+    public List<String> getEvacuationZones() { return evacuationZones; }
+    public void setEvacuationZones(List<String> evacuationZones) { this.evacuationZones = evacuationZones; }
+    public List<String> getStatusEffects() { return statusEffects; }
+    public void setStatusEffects(List<String> statusEffects) { this.statusEffects = statusEffects; }
+    public List<String> getUnparsedData() { return unparsedData; }
+    public void setUnparsedData(List<String> unparsedData) { this.unparsedData = unparsedData != null ? unparsedData : new ArrayList<>(); }
+    public void addUnparsedData(String value) { if (value != null && !value.isBlank()) unparsedData.add(value); }
 }
